2e10136f86fdc910c4541cf54f64866d87f3c8ef
Danio Melo <danilomarquesdemelo@gmail.com>
2023-09-01 12:52:33 -0300

    Adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-export default function Home () {
-    return (
-        <h1>Teste</h1>
-    )
+export default function Home() {
+  return <h1>Teste</h1>;
+}
+
+function teste() {
+  console.log();
 }
