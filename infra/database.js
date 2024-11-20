83160cc2563b3016f102ae7e12101d590a1757b6
DaniloMelo <danilomarquesdemelo@gmail.com>
2024-11-19 21:12:52 -0300

    cria arquivo compose.yaml e database.js

---
+++ infra/database.js
@@ -0,0 +1,25 @@
+import { Client } from "pg"
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD
+  })
+
+  try {
+    await client.connect()
+    const res = await client.query(queryObject)
+    return res
+  } catch (error) {
+    console.error(error)
+  } finally {
+    await client.end()
+  }
+}
+
+export default {
+  query: query
+}
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-export default function status(req, res) {
-  console.log(req)
+import database from "../../../../infra/database.js"
+
+export default async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as teste;")
+  console.log(result.rows[0])
+
+  console.log(process.env.NODE_ENV)
+
   return res.status(200).json({
     message: "Este é um teste",
   });
