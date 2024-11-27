1288ad059d1dcde4900cb3bec82a571a09eb7544
DaniloMelo <danilomarquesdemelo@gmail.com>
2024-11-27 19:30:14 -0300

    Opened connections on /migrations endpoint challenge

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,6 +3,44 @@ import { join } from "node:path"
 import database from "infra/database"
 
 export default async function migrations(req, res) {
+  // const dbClient = await database.getNewClient()
+
+  // const defaultMigrationOptions = {
+  //   dbClient: dbClient,
+  //   dryRun: true,
+  //   dir: join("infra", "migrations"),
+  //   direction: "up",
+  //   verbose: true,
+  //   migrationsTable: "pgmigrations"
+  // }
+
+  // if (req.method === "GET") {
+  //   const pendingMigrations = await migrationRunner(defaultMigrationOptions)
+  //   dbClient.end()
+  //   return res.status(200).json(pendingMigrations)
+  // }
+
+  // if (req.method === "POST") {
+  //   const migratedMigrations = await migrationRunner({
+  //     ...defaultMigrationOptions,
+  //     dryRun: false
+  //   })
+
+  //   dbClient.end()
+
+  //   if (migratedMigrations.length > 0) {
+  //     return res.status(201).json(migratedMigrations)
+  //   }
+
+  //   return res.status(200).json(migratedMigrations)
+  // }
+
+  // res.status(405).end()
+
+
+
+
+  // ----------- 
   const dbClient = await database.getNewClient()
 
   const defaultMigrationOptions = {
@@ -14,26 +52,38 @@ export default async function migrations(req, res) {
     migrationsTable: "pgmigrations"
   }
 
-  if (req.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions)
-    dbClient.end()
-    return res.status(200).json(pendingMigrations)
-  }
-
-  if (req.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false
-    })
-
-    dbClient.end()
-
-    if (migratedMigrations.length > 0) {
-      return res.status(201).json(migratedMigrations)
+  try {
+    if (
+      req.method === "DELETE" ||
+      req.method === "PUT" ||
+      req.method === "PATCH"
+    ) {
+      return res.status(405).end()
     }
 
-    return res.status(200).json(migratedMigrations)
-  }
+    if (req.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions)
+      // dbClient.end()
+      return res.status(200).json(pendingMigrations)
+    }
 
-  res.status(405).end()
+    if (req.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false
+      })
+      // dbClient.end()
+      if (migratedMigrations.length > 0) {
+        return res.status(201).json(migratedMigrations)
+      }
+
+      return res.status(200).json(migratedMigrations)
+    }
+
+  } catch (error) {
+    console.error(error)
+
+  } finally {
+    dbClient.end()
+  }
 }
--- teste.js
+++
@@ -1,2 +0,0 @@
-console.log("isso é um teste 1")
-console.log("alterei este arquivo na branch fix-migrations-endpoint2")
