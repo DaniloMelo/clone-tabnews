f9790417b02a2d6731bccf616bb603cfec51f1b6
DaniloMelo <danilomarquesdemelo@gmail.com>
2024-11-27 21:30:42 -0300

    refactor opened connections on /migrations endpoint challenge

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,67 +3,30 @@ import { join } from "node:path"
 import database from "infra/database"
 
 export default async function migrations(req, res) {
-  // const dbClient = await database.getNewClient()
+  const allowedMethods = ["GET", "POST"]
 
-  // const defaultMigrationOptions = {
-  //   dbClient: dbClient,
-  //   dryRun: true,
-  //   dir: join("infra", "migrations"),
-  //   direction: "up",
-  //   verbose: true,
-  //   migrationsTable: "pgmigrations"
-  // }
-
-  // if (req.method === "GET") {
-  //   const pendingMigrations = await migrationRunner(defaultMigrationOptions)
-  //   dbClient.end()
-  //   return res.status(200).json(pendingMigrations)
-  // }
-
-  // if (req.method === "POST") {
-  //   const migratedMigrations = await migrationRunner({
-  //     ...defaultMigrationOptions,
-  //     dryRun: false
-  //   })
-
-  //   dbClient.end()
-
-  //   if (migratedMigrations.length > 0) {
-  //     return res.status(201).json(migratedMigrations)
-  //   }
-
-  //   return res.status(200).json(migratedMigrations)
-  // }
-
-  // res.status(405).end()
-
-
-
-
-  // ----------- 
-  const dbClient = await database.getNewClient()
-
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations"
+  if (!allowedMethods.includes(req.method)) {
+    return res.status(405).json({
+      error: `Method "${req.method} not allowed"`
+    })
   }
 
+  let dbClient
+
   try {
-    if (
-      req.method === "DELETE" ||
-      req.method === "PUT" ||
-      req.method === "PATCH"
-    ) {
-      return res.status(405).end()
+    dbClient = await database.getNewClient()
+
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations"
     }
 
     if (req.method === "GET") {
       const pendingMigrations = await migrationRunner(defaultMigrationOptions)
-      // dbClient.end()
       return res.status(200).json(pendingMigrations)
     }
 
@@ -72,7 +35,7 @@ export default async function migrations(req, res) {
         ...defaultMigrationOptions,
         dryRun: false
       })
-      // dbClient.end()
+
       if (migratedMigrations.length > 0) {
         return res.status(201).json(migratedMigrations)
       }
@@ -82,8 +45,9 @@ export default async function migrations(req, res) {
 
   } catch (error) {
     console.error(error)
+    throw error
 
   } finally {
-    dbClient.end()
+    await dbClient.end()
   }
 }
