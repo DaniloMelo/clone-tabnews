cc72aae17d85921a9d5977dcf56c1d96112199d3
DaniloMelo <danilomarquesdemelo@gmail.com>
2024-12-05 19:00:40 -0300

    fix code linting

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,19 +1,19 @@
-const { exec } = require("node:child_process")
+const { exec } = require("node:child_process");
 
 function checkPostgres() {
-  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn)
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
   function handleReturn(error, stdout) {
     if (stdout.search("accepting connections") === -1) {
-      checkPostgres()
-      process.stdout.write(".")
-      return
+      checkPostgres();
+      process.stdout.write(".");
+      return;
     }
 
-    console.log("\n\n🟢 Postgres está pronto e aceitando conexões.\n")
+    console.log("\n\n🟢 Postgres está pronto e aceitando conexões.\n");
   }
 }
 
-process.stdout.write("\n\n🔴 Aguardando Postgres aceitar conexões")
+process.stdout.write("\n\n🔴 Aguardando Postgres aceitar conexões");
 
-checkPostgres()
+checkPostgres();
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,25 +1,28 @@
-import database from "infra/database.js"
-import orchestrator from "tests/orchestrator.js"
+import database from "infra/database.js";
+import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
-  await orchestrator.waitForAllServices()
-  await database.query("drop schema public cascade; create schema public;")
-})
+  await orchestrator.waitForAllServices();
+  await database.query("drop schema public cascade; create schema public;");
+});
 
 test("Post to api/v1/migrations should return 200", async () => {
   const firstResponse = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST"
-  })
-  const firstResponseBody = await firstResponse.json()
-  expect(firstResponse.status).toBe(201)
-  expect(Array.isArray(firstResponseBody)).toBe(true)
-  expect(firstResponseBody.length).toBeGreaterThan(0)
+    method: "POST",
+  });
+  const firstResponseBody = await firstResponse.json();
+  expect(firstResponse.status).toBe(201);
+  expect(Array.isArray(firstResponseBody)).toBe(true);
+  expect(firstResponseBody.length).toBeGreaterThan(0);
 
-  const secondResponse = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST"
-  })
-  const secondResponseBody = await secondResponse.json()
-  expect(secondResponse.status).toBe(200)
-  expect(Array.isArray(secondResponseBody)).toBe(true)
-  expect(secondResponseBody.length).toBe(0)
-})
+  const secondResponse = await fetch(
+    "http://localhost:3000/api/v1/migrations",
+    {
+      method: "POST",
+    },
+  );
+  const secondResponseBody = await secondResponse.json();
+  expect(secondResponse.status).toBe(200);
+  expect(Array.isArray(secondResponseBody)).toBe(true);
+  expect(secondResponseBody.length).toBe(0);
+});
