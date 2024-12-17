196a2576a5cb65f3abfe02827724180ffaf58b2c
DaniloMelo <danilomarquesdemelo@gmail.com>
2024-12-16 21:43:34 -0300

    style: corrects missing new line in end of files

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,6 @@
 // These styles apply to every route in the application
-import 'styles/globals.css'
+import "styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
