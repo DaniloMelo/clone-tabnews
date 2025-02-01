093fd36eda8fc9a006a4018ba6e2bb943565b206
DaniloMelo <danilomarquesdemelo@gmail.com>
2025-01-31 21:20:28 -0300

    refactor: use `next-connext` in `/status` and `/migrations` endpoints

---
+++ tests/integration/api/v1/status/post.test.js
@@ -0,0 +1,25 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
+describe("POST to /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status", {
+        method: "POST",
+      });
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(405);
+
+      expect(responseBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Método HTTP não permitido para este endpoint.",
+        action: "Verifique se o método HTTP enviado é válido.",
+        status_code: 405,
+      });
+    });
+  });
+});
